066f80d0d858d8fd039d3e08ba14c2175a9e0fe1
ehsan-mad <saadkhan420000@gmail.com>
2025-08-30 22:10:55 +0600

    Add Docker deployment configuration for Render with Nginx

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running CockroachDB migrations..."
+php artisan cockroach:migrate
